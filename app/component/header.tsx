b3da27adf5a0782c5c369daf781c6ff51ce1a6b3
Angelicabassey <angelicabassey44@gmail.com>
2024-03-19 12:08:18 +0100

    just work

--- app/component/header.tsx
+++ app/component/header.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from 'next/image';
 import Headerimg from "./headerimg";
 
@@ -8,21 +9,21 @@ export default function Header() {
                 <div>
                     <Image
                         src="/logo.png"
-                        alt="Next.js Logo"
+                        alt="logo pion"
                         width={150}
                         height={150}
                     />
                 </div>
                 <div className="flex items-center gap-20">
-                    <p className='hover:text-main'><a className='text-main' href="">Home</a></p>
-                    <p className='hover:text-main'><a href="">About Us</a></p>
-                    <p className='hover:text-main'><a href="">Roadmap</a></p>
-                    <p className='hover:text-main'><a href="">Contact Us</a></p>
+                    <p className='hover:text-main'>Home</p>
+                    <p className='hover:text-main'>About Us</p>
+                    <p className='hover:text-main'>Roadmap</p>
+                    <p className='hover:text-main'>Contact Us</p>
                 </div>
                 <div className="bg-main px-6 py-2 rounded-md">
                     <div className='flex items-center gap-2'>
                         <p className='rounded-md font-normal xl:text-base sm:text-sm text-white'>Let's Earn</p>
-                        <Headerimg />
+                        <Headerimg/>
                     </div>
                 </div>
             </div>
