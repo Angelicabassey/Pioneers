65c21a589ca817e9d8a0d197344799a4aab4b972
Angelicabassey <angelicabassey44@gmail.com>
2024-03-19 12:34:56 +0100

    hsi

--- app/component/firstpage.tsx
+++ app/component/firstpage.tsx
@@ -9,9 +9,9 @@ export default function Firstpage() {
                 <div className="flex flex-col gap-6 w-2/4">
                     <h3 className="font-medium text-main text-5xl">Discover Solana: <br/>Learn, <p className="text-primarytext">Earn</p> and Thrive <br/> in the Web3 Economy</h3>
                     <p className="w-94 font-semibold text-sm">Lorem ipsum dolor sit amet consectetur. Vel blandit tempor id nunc et auctor tellus eu nisi. Eu sem tellus magna vulputate. Neque etiam sit molestie aenean placerat ornare quis. Urna duis morbi sit pretium et.</p>
-                    <div className=" bg-main px-6 py-2 rounded-md w-4/12">
-                        <link className='flex items-center text-center' href="./login">
-                            <p className='px-5 font-normal text-center xl:text-base sm:text-sm text-white'>Let's Earn</p>
+                    <div className="bg-main px-6 py-2 rounded-md w-4/12">
+                        <link className="flex items-center text-center" href="./login">
+                            <p className="px-5 font-normal text-center xl:text-base sm:text-sm text-white">Let's Earn</p>
                             <Headerimg />
                         </link>
                     </div>
