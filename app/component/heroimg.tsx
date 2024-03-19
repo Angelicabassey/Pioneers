f649182f5473b99b7ffa66419b3152acd782a4a7
Angelicabassey <angelicabassey44@gmail.com>
2024-03-19 12:04:09 +0100

    work

--- app/component/heroimg.tsx
+++ app/component/heroimg.tsx
@@ -1,16 +1,16 @@
-  
-  import Image from 'next/image'
 
-  export default function Hero() {
-    return (
-     <div className='flex- justify-center- items-center- h-screen-'>
-       <Image
+import Image from 'next/image'
+
+export default function Heroimg() {
+  return (
+    <div className=''>
+      <Image
         src="/hero.png"
         width={400}
         height={400}
         alt="Picture of the author"
-        />
-     </div>
-    )
-  }
-  
+      />
+    </div>
+  )
+}
+
